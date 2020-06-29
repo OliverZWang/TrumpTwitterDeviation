12b06d957be3736fb3333eaaa6496c051bb69dd5
AnonymousUserOliver <zw1400@nyu.edu>
2020-06-29 17:46:02 -0400

    add TrendPageScraper class

---
+++ trends_scraper/TrendPageScraper.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+
+class TrendPageScraper():
+    def __init__(self, url, dest_twitter, dest_google):
+        self.url = url
+        self.dest_twitter = dest_twitter
+        self.dest_google = dest_google
+        self.twitter_trends = ''
+        self.google_trends = ''
+        self.get_trends()
+        self.write_trends()
+
+    def get_trends(self):
+        self.page = requests.get(self.url)
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
+
+        iterator = 0
+        all_ol = self.soup.find_all('ol') 
+        ol_len = len(all_ol)
+
+        while iterator < ol_len:
+            ol_tag = all_ol[iterator]
+            for item in ol_tag:
+                if item != '\n':
+                    # print(item.get_text())
+                    # print('-'*20)
+                    if iterator < 2:
+                        self.twitter_trends += item.get_text()+','
+                    else:
+                        self.google_trends += item.get_text()+','
+            iterator += 1
+        self.twitter_trends = self.twitter_trends[:-1]
+        self.google_trends = self.google_trends[:-1]
+    
+    def write_trends(self):
+        self.f_t = open(self.dest_twitter, "w")
+        self.f_t.write(self.twitter_trends)
+        self.f_t.close()
+
+        self.f_g = open(self.dest_google, "w")
+        self.f_g.write(self.google_trends) 
+        self.f_g.close()
+        
+        # print(self.f.read())
+
+        
+
+if __name__ == '__main__':
+    #testing
+    scraper = TrendPageScraper('https://us.trend-calendar.com/trend/2020-06-28.html', "./2020-06-28-twitter.txt", "./2020-06-28-google.txt")
+    # print(scraper.twitter_trends)
+    # print(scraper.google_trends)
+
+
+
+
--- twitter_trends/twitter_trends_starter.py
+++ twitter_trends/twitter_trends_starter.py
@@ -13,6 +13,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 trends_us = api.trends_place(23424977)
+# trends_us = api.trends_place(1)
+# api.update_status('Updating using OAuth authentication via Tweepy!')
 print(trends_us)
 trends_us = json.loads(json.dumps(trends_us, indent=1))
 print("Current trend in the United States: ")
