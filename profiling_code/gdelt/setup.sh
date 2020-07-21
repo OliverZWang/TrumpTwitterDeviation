93af5e23687acf1b8cc8cf7ff4d2eebca0fc4ed2
Carl Barbee <carlb15@users.noreply.github.com>
2020-07-21 13:40:57 -0400

    Update setup.sh

--- profiling_code/gdelt/setup.sh
+++ profiling_code/gdelt/setup.sh
@@ -2,10 +2,7 @@
 
 # Create new directory structure in HDFS
 hdfs dfs -mkdir rbda_proj
-hdfs dfs -mkdir rbda_proj/profile_in
-
-# Populate the input directory in HDFS with the input file
-hdfs dfs -put input.txt rbda_proj/profile_in
+hdfs dfs -mkdir rbda_proj/profile_out
 
 # Verify what is in the input data directory
 hdfs dfs -ls rbda_proj/profile_in
